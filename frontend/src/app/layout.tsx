3b53afb970e4d0f8d70fce38c6c106057bbd41dc
Okkun555 <y.okuno.btm@gmail.com>
2025-05-11 23:08:00 +0900

    smarthr-uiを適用する

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import "smarthr-ui/smarthr-ui.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
---
+++ frontend/src/components/elements/index.ts
@@ -0,0 +1,11 @@
+/**
+ * パッチ修正などが必要になった際の影響範囲を最小限にするために
+ * SmartHR UIのコンポーネントがすべて当ファイルを経由してimportします。
+ *
+ * @see https://github.com/kufu/smarthr-ui
+ */
+export {
+  FormControl,
+  Input,
+  Button,
+} from "smarthr-ui";
