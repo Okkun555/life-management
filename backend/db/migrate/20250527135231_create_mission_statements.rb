e4b258d1485e2900f7791da05d5db95ca13568f6
Okkun555 <y.okuno.btm@gmail.com>
2025-07-05 20:19:12 +0900

    不要なマイグレーションを削除

--- backend/db/migrate/20250527135231_create_mission_statements.rb
+++
@@ -1,8 +0,0 @@
-class CreateMissionStatements < ActiveRecord::Migration[7.2]
-  def change
-    create_table :mission_statements do |t|
-      t.text :content, null: false, comment: "ミッションステートメント"
-      t.timestamps
-    end
-  end
-end
