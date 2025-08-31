128e897eae60e5602b58c93c824ef15ec21be7d5
Okkun555 <y.okuno.btm@gmail.com>
2025-08-31 10:57:34 +0900

    フロントエンドでAPIレスポンスをスネークケースからキャメルケースに置き換える

--- frontend/src/libs/api/fetcher.ts
+++ frontend/src/libs/api/fetcher.ts
@@ -1,19 +1,24 @@
 import axios from "axios";
+import { camelizeKeys } from "humps";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
-    "Accept": "application/json",
-    "X-Dev-User-UID": process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_DEV_USER_UID : undefined, // 開発環境用のログインIDを添付
-  }
+    Accept: "application/json",
+    "X-Dev-User-UID":
+      process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_DEV_USER_UID : undefined, // 開発環境用のログインIDを添付
+  },
 });
 
-export const fetcher = <T>(url: string): Promise<T> => axiosInstance.get(url).then(res => res.data);
+export const fetcher = <T>(url: string): Promise<T> =>
+  axiosInstance.get(url).then((res) => camelizeKeys(res.data) as T);
 
-export const postRequest = <T, U>(url: string, { arg }: { arg: U }): Promise<T> => axiosInstance.post(url, arg).then(res => res.data);
+export const postRequest = <T, U>(url: string, { arg }: { arg: U }): Promise<T> =>
+  axiosInstance.post(url, arg).then((res) => camelizeKeys(res.data) as T);
 
-export const putRequest = <T, U>(url: string, { arg }: { arg: U }): Promise<T> => axiosInstance.put(url, arg).then(res => res.data);
+export const putRequest = <T, U>(url: string, { arg }: { arg: U }): Promise<T> =>
+  axiosInstance.put(url, arg).then((res) => camelizeKeys(res.data) as T);
 
-export const deleteRequest = <T>(url: string): Promise<T> => axiosInstance.delete(url).then(res => res.data);
-
+export const deleteRequest = <T>(url: string): Promise<T> =>
+  axiosInstance.delete(url).then((res) => camelizeKeys(res.data) as T);
