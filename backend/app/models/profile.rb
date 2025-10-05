8b2f5992eb06350197ccc196d24f3c540d9a7cfa
Okkun555 <y.okuno.btm@gmail.com>
2025-10-05 18:47:04 +0900

    年齢計算ロジックを追加

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -7,4 +7,17 @@ class Profile < ApplicationRecord
   validates :is_public, inclusion: { in: [true, false] }
 
   enum :sex, { secret: 0, man: 1, woman: 2 }
+
+  # 年齢を整数で返す
+  # デフォルトは今日時点（対象日時点での年齢を計算する場合、引数に対象日を指定）
+  def age(as_of: Date.current)
+    return nil unless birthday
+
+    years_diff = as_of.year - birthday.year
+
+    # 誕生日を迎えていない場合は 1 年引く
+    had_birthday = (as_of.month > birthday.month) || (as_of.month == birthday.month && as_of.day >= birthday.day)
+    years_diff -= 1 unless had_birthday
+    years_diff
+  end
 end
--- backend/spec/models/profile_spec.rb
+++ backend/spec/models/profile_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#age' do
+    let(:user) { create(:user) }
+    let(:target_day) { Date.new(2025, 10, 5) }
+
+    context '誕生日が登録されていない場合' do
+      let(:profile) { described_class.new(user:, birthday: nil) }
+
+      it 'nilを返却する' do
+        expect(profile.age(as_of: target_day)).to be_nil
+      end
+    end
+
+    context '誕生日が登録されている場合' do
+      context 'その年の誕生日を既に迎えている場合' do
+        let(:profile) { described_class.new(user:, birthday: Date.new(1990, 10, 1)) }
+
+        it '正しい年齢を返却する' do
+          expect(profile.age(as_of: target_day)).to eq 35
+        end
+      end
+
+      context 'その年の誕生日当日の場合' do
+        let(:profile) { described_class.new(user:, birthday: Date.new(1990, 10, 5)) }
+
+        it '正しい年齢を返却する' do
+          expect(profile.age(as_of: target_day)).to eq 35
+        end
+      end
+
+      context 'その年の誕生日を迎えていない場合' do
+        let(:profile) { described_class.new(user:, birthday: Date.new(1990, 10, 10)) }
+
+        it '正しい年齢を返却する' do
+          expect(profile.age(as_of: target_day)).to eq 34
+        end
+      end
+    end
+  end
 end
